47f902727116de508e6cf0679e1ea99294d6d7a9
_v3 <116607327+marsianjohncarter@users.noreply.github.com>
2025-06-07 13:54:42 -0700

    Fix footer UI and hero UI

--- src/app/(components)/hero/hero.tsx
+++ src/app/(components)/hero/hero.tsx
@@ -69,28 +69,22 @@ function Hero() {
                             </span>
                         </h1>
 
-                        <p className="text-lg md:text-md leading-relaxed tracking-tight text-muted-foreground max-w-2xl text-center font-serif">
-                            Undictify is the social media un-addictor — a fresh
-                            approach to helping you take back control.
-                            Thoughtfully designed to shift your relationship
-                            with digital life, it’s not just another app — it’s
-                            a quiet rebellion.
+                        <p className="text-sm md:text-lg leading-relaxed tracking-tight text-muted-foreground max-w-2xl text-center font-serif">
+                            Undictify: A quiet rebellion against digital
+                            addiction, helping you reclaim control of your
+                            online life.
                         </p>
                     </div>
                     <div className="flex flex-row gap-3">
                         <Link href={'/dashboard'}>
-                            <Button
-                                size="lg"
-                                className="gap-4"
-                                variant="outline"
-                            >
+                            <Button className="gap-4 cursor-pointer" variant="outline">
                                 Go to dashboard{' '}
                                 <LayoutDashboard className="w-4 h-4" />
                             </Button>
                         </Link>
 
                         <Link href={'/signup'}>
-                            <Button size="lg" className="gap-4 cursor-pointer">
+                            <Button className="gap-4 cursor-pointer">
                                 Sign up here <MoveRight className="w-4 h-4" />
                             </Button>
                         </Link>
