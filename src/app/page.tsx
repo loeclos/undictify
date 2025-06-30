f981fb5fac1dd7c9c8cbe1199bc1be7a76aadf0a
_v3 <116607327+marsianjohncarter@users.noreply.github.com>
2025-06-30 14:38:31 -0700

    Fix deploy erros

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-import Navbar from '@/app/(components)/navbar/navbar';
-import { Hero } from '@/app/(components)/hero/hero';
-import Footer from '@/app/(components)/footer/footer';
 
-const navItems = [
-    { id: 'home', text: 'Home', url: '/' },
-    { id: 'blog', text: 'Blog', url: '/blog' },
-    { id: 'dashboard', text: 'Dashboard', url: '/dashboard' },
-];
+import { Hero } from '@/app/(components)/hero/hero';
 
 export default function Page() {
     return (
